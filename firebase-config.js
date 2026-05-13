eaebeb5f701685cb10dc517e0b73ce14c06fec5f
ntxm210-dev <ntxm210@gmail.com>
2026-05-13 20:36:14 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCm1hP13NoImY8A8qIzj5H16TyvkHEhQAw",
+  authDomain: "luubutngayxanh.firebaseapp.com",
+  projectId: "luubutngayxanh",
+  storageBucket: "luubutngayxanh.firebasestorage.app",
+  messagingSenderId: "306995751080",
+  appId: "1:306995751080:web:c6af887ec14d79ae58fa9d",
+  measurementId: "G-T7HC2JPWBP"
+};
+
+// Initialize Firebase
